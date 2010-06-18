13619103e675172ee63932f16153d690aa80f781
David Sommers <databyte@gmail.com>
2010-06-17 23:21:24 -0400

    removed leftover code

--- jquery.quicksand.js
+++ jquery.quicksand.js
@@ -285,8 +285,6 @@ Github site: http://github.com/razorjack/quicksand
                     if (options.maxWidth == 0 || destElement.offset().left < options.maxWidth) {
                       animationQueue.push({element: $(d),
                                            animation: animationOptions});
-                    } else {
-                      
                     }
                 }
             });
