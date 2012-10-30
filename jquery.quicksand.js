e474b84bdcf5c3e4749fa4e216a8c81c4e7270ee
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2012-10-30 10:14:56 -0200

    * small typo fix

--- jquery.quicksand.js
+++ jquery.quicksand.js
@@ -407,7 +407,7 @@ Github site: http://github.com/razorjack/quicksand
                 	}
 				}
             } else {
-                $toDelete = $sourceParent.find(optins.selector);
+                $toDelete = $sourceParent.find(options.selector);
                 $sourceParent.prepend($dest.find(options.selector));
                 for (i = 0; i < animationQueue.length; i++) {
                     if (animationQueue[i].dest && animationQueue[i].style) {
